36bed72af86cf8a65d437bceb8899b4693f8072e
Bailey Latimer <latimerdesignco@gmail.com>
2022-04-20 14:00:10 -0700

    added social section

--- src/pages/index.js
+++ src/pages/index.js
@@ -135,6 +135,78 @@ return (
           </div>
         </div>
       </div>
+
+      <div class="border radius-lg width-100% margin-top-md">
+        <div class="grid border-bottom">
+          <div class="flex justify-between col-4 items-center padding-sm border-right">
+            <svg width="124" height="55" viewBox="0 0 124 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g clip-path="url(#clip0_2542_3733)">
+  <line x1="-47.6289" y1="80.4559" x2="16.9591" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="-35.835" y1="80.4559" x2="28.7531" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="-24.0411" y1="80.4559" x2="40.547" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="-12.2471" y1="80.4559" x2="52.341" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="-0.453165" y1="80.4559" x2="64.1349" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="11.3408" y1="80.4559" x2="75.9289" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="23.1347" y1="80.4559" x2="87.7228" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="34.9287" y1="80.4559" x2="99.5167" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="46.7226" y1="80.4559" x2="111.311" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="58.5166" y1="80.4559" x2="123.105" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="70.3105" y1="80.4559" x2="134.899" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="82.1045" y1="80.4559" x2="146.693" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="93.8984" y1="80.4559" x2="158.486" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="105.692" y1="80.4559" x2="170.28" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  <line x1="117.486" y1="80.4559" x2="182.074" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+  </g>
+  <defs>
+  <clipPath id="clip0_2542_3733">
+  <rect width="124" height="54" fill="white" transform="translate(0 0.5)"/>
+  </clipPath>
+  </defs>
+            </svg>
+            <h2 class="text-xxxl">002</h2>
+            <svg width="124" height="55" viewBox="0 0 124 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0_2542_3733)">
+              <line x1="-47.6289" y1="80.4559" x2="16.9591" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="-35.835" y1="80.4559" x2="28.7531" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="-24.0411" y1="80.4559" x2="40.547" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="-12.2471" y1="80.4559" x2="52.341" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="-0.453165" y1="80.4559" x2="64.1349" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="11.3408" y1="80.4559" x2="75.9289" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="23.1347" y1="80.4559" x2="87.7228" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="34.9287" y1="80.4559" x2="99.5167" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="46.7226" y1="80.4559" x2="111.311" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="58.5166" y1="80.4559" x2="123.105" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="70.3105" y1="80.4559" x2="134.899" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="82.1045" y1="80.4559" x2="146.693" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="93.8984" y1="80.4559" x2="158.486" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="105.692" y1="80.4559" x2="170.28" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              <line x1="117.486" y1="80.4559" x2="182.074" y2="-17.5563" stroke="#DFDFDF" stroke-width="2"/>
+              </g>
+              <defs>
+              <clipPath id="clip0_2542_3733">
+              <rect width="124" height="54" fill="white" transform="translate(0 0.5)"/>
+              </clipPath>
+              </defs>
+            </svg>
+          </div>
+          <div class="col-8 flex justify-center padding-sm">
+            <h2 class="text-xxxl text-nowrap">Join the community</h2>
+          </div>
+        </div>
+        <div class="grid border-bottom">
+          <div class="col-12 border-right card">
+            
+          </div>
+        </div>
+        <div class="grid border-top">
+          <div class="col-6">
+            <a class="btn btn--primary btn--lg height-100%" href="openseas.io" target="_blank">Discord</a>
+          </div>
+          <div class="col-6">
+            <a class="btn btn--primary btn--lg height-100%" href="openseas.io" target="_blank">Twitter</a>
+          </div>
+        </div>
+      </div>
     </section>
 
 <div class="snowflakes" aria-hidden="true">
