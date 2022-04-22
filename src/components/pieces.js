52439957e5e7ac4164c40adb9ce483fe5eca0084
Bailey Latimer <latimerdesignco@gmail.com>
2022-04-21 18:35:44 -0700

    converted pieces to be responsive on mobile

--- src/components/pieces.js
+++ src/components/pieces.js
@@ -23,24 +23,30 @@ class Pieces extends React.Component {
           </div>
         </div>
         <div class="grid border-bottom">
-          <div class="col-3 border-right card">
-            
-          </div>
-          <div class="col-3 border-right card">
-            
-          </div>
-          <div class="col-3 border-right card">
+          <div class="flex col-6@lg col-12">
+            <div class="width-50% border card">
+              
+            </div>
           
-          </div>
-          <div class="col-3 border-right card">
+              <div class="width-50% border card">
+                
+              </div>
+            </div>
+            <div class="flex col-6@lg col-12">
+              <div class="width-50% border card">
+                
+              </div>
           
-          </div>
+              <div class="width-50% border card">
+                
+              </div>
+            </div>
         </div>
-        <div class="grid border-top">
-          <div class="col-6 padding-sm">
+        <div class="grid border-top flex">
+          <div class="col-6@md col-12 padding-sm">
             <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate libero vitae feugiat diam leo. Venenatis sapien in malesuada.</p>
           </div>
-          <div class="col-6">
+          <div class="col-6@md col-12">
             <a class="btn btn--primary btn--lg height-100%" href="openseas.io" target="_blank">See full collection</a>
           </div>
         </div>
